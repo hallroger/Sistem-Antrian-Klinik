6b60d5f243d09d3c9882e7862f27029b612caaf8
hallroger <dewamadeaditya@student.telkomuniversity.ac.id>
2021-06-10 10:22:05 +0800

    update feedback form
    melakukan update collumn pada bagian oengisian feedback

--- Tubes/application/views/feedback.php
+++ Tubes/application/views/feedback.php
@@ -12,22 +12,36 @@ if($this->session->flashdata('message')){
                         Feedback Sistem
                     </h4>
                 </div>
+                <!--<div class="modal-body">
+                     <form action="<?php echo base_url('home/feedback'); ?>" method="post">
+                    <input type="hidden" class="form-control" placeholder="Role" name="role" value="Feedback">
+                     <div class="form-group has-feedback">
+                    <input type="text" class="form-control" placeholder="Nama" name="nama_feedback">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                </div>
+                <div class="form-group has-feedback">
+                    <textarea name=" Feedback" rows="8" cols="40" placeholder="Feedback" name="feedback"></textarea>
+                   <!-- <span class="glyphicon glyphicon-home form-control-feedback"></span> 
+                </div>
+                 
+                        <button type="submit" id="btnSignUp" class="btn btn-primary" style="width: 127px;">Submit</button>
+                    </form> 
+                    
+                </div>-->
                 <div class="modal-body">
                      <form action="<?php echo base_url('home/feedback'); ?>" method="post">
                     <input type="hidden" class="form-control" placeholder="Role" name="role" value="Feedback">
                      <div class="form-group has-feedback">
-                    <input type="text" class="form-control" placeholder="Nama" name="nama">
+                    <input type="text" class="form-control" placeholder="Nama" name="nama_feedback">
                     <span class="glyphicon glyphicon-user form-control-feedback"></span>
                 </div>
-                <div class="form-group has-feedback">
-                    <textarea name=" Feedback" rows="8" cols="40"></textarea>
-                    <span class="glyphicon glyphicon-home form-control-feedback"></span>
-                </div>
-                 
-                        <button type="submit" id="btnSignUp" class="btn btn-primary" style="width: 127px;">Submit</button>
+                    <div class="form-group has-feedback">
+                    <textarea placeholder = "Feedback" name="feedback" required></textarea>
+                    <input type="submit">
+                </div>   
                     </form>
                     
-                </div>
+                </div>    
             </div>
             </div>
     </div>
